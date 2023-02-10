9595e9c61c51e49cd1f446d821abad9aff5ea7a3
Wojtekban <wojtekban@op.pl>
2023-02-10 22:01:43 +0100

    Zadanie z dnia nr 5

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,40 +1,42 @@
-﻿// Wykonanie Zadania nr 4
-//"Kobieta poniżej 30 lat";
-//"Ewa lat 33"
-//"Niepełnoletni mężczyzna";
-//"Chłopiec"
-//"Kobieta w wieku emerytalnym"
-//"Mężczyzna w wieku emerytalnym"
+﻿// Wykonanie Zadania nr 5
+// Przygotuj program, który policzy ile jakich cyfr
+// występuje w podanej liczbie.
+//
+// Przyklad
+// Wynik dla liczby: 4566
+// 0 => 0
+// 1 => 0
+// 2 => 0
+// 3 => 0
+// 4 => 1
+// 5 => 1
+// 6 => 2
+// 7 => 0
+// 8 => 0
+// 9 => 0
 
-string myName = "Kinga";
-bool isWomen = true;
-int myAge = 28;
 
-if (isWomen == true && myAge < 30)    //kobieta poniżej 30 lat
-            {
-                Console.WriteLine("Kobieta poniżej 30 lat.");
-            }
-else if (myName == "Kinga" && myAge == 33)   //"Kinga lat 33"
-            {
-                Console.WriteLine("Kinga lat 33");
-            }
-else if (isWomen == false && myAge >=12 && myAge < 18)   //"Niepełnoletni mężczyzna" 
-            {
-                Console.WriteLine("Niepełnoletni mężczyzna."); 
-            }
-else if (isWomen == false && myAge < 12)   //"Chłopiec"
-            {
-                Console.WriteLine("Chlopiec.");
-            }
-else if (isWomen == true && myAge >= 60)   //"Kobieta w wieku emerytalnym"
-            {
-                Console.WriteLine("Kobieta w wieku emerytalnym.");
-            }
-else if (isWomen == false && myAge >= 75)   //"Mężczyzna w wieku emerytalnym"
-            {
-            Console.WriteLine("Mężczyzna w wieku emerytalnym.");
-            }
-else
+//int number = 456677;
+Console.WriteLine("Podaj liczbę:");
+string number = Console.ReadLine();
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+
+int[] results = new int[10];
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Nieznana osoba"); //myAge = 18 && myAge = 30 spełnia warunek
+    for (int i = 0; i <= results.Length; i++)
+    {
+        int num = letter - '0';
+        if (num == i)
+        {
+            results[i]++;
+        }
+
+    }
+}
+for (int j = 0; j < results.Length; j++)
+{
+    Console.WriteLine("Liczba " + j + " występuje " + "=> " + results[j] + " razy");
 }
