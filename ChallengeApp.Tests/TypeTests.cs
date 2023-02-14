7e0acdc1e629522cd0d852cf5212174099159547
Wojtekban <wojtekban@op.pl>
2023-02-14 20:02:08 +0100

    Zadanie z dnia nr 8

---
+++ ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,69 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TestWhenToAddIntegersThenCompareIfTheyAreEqual()
+        {
+            //arrange
+            float number1 = 10;
+            float number2 = 10;
+
+            //act
+
+
+            //assert
+            Assert.AreEqual(number1, number2);
+
+        }
+
+        [Test]
+        public void TestWhenToAddDecimalNumbersThenCompareIfTheyAreEqual()
+        {
+            //arrange
+            double number1 = 3.14;
+            double number2 = 3.14;
+
+            //act
+
+
+            //assert
+            Assert.AreEqual(number1, number2);
+
+        }
+
+        [Test]
+        public void WhenToAddNumbersThenCompareIfTheyAreEqual()
+        {
+            //arrange
+            int number1 = 1;
+            int number2 = 1;
+
+            //act
+
+
+            //assert
+            Assert.AreEqual(number1, number2);
+
+        }
+
+        [Test]
+        public void WhenToAddUsersThenCompareIfTheyAreEqual()
+        {
+            //arrange
+            var user1 = GetEmployee("Adam", "Wilczewski", 30);
+            var user2 = GetEmployee("Kasia", "Lisewska", 25);
+
+            //act
+
+
+            //assert
+            Assert.AreNotEqual(user1, user2);
+
+        }
+        private Employee GetEmployee(string name, string surName, int age)
+        {
+            return new Employee(name, surName, age);
+        }
+    }
+}
