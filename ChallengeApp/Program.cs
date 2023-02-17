813fa8fa75013fd0649203e9bc284b0baa206963
Wojtekban <wojtekban@op.pl>
2023-02-17 19:03:14 +0100

    Czysty kod  dzień 12

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -9,26 +9,3 @@ employee.AddGrade(14);
 employee.AddGrade(99);
 employee.AddGrade(2.20E+23);
 
-Console.WriteLine("Wynik dla ForEach = ");
-var statistics = employee.GetStatisticsForEach();
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-
-Console.WriteLine("Wynik dla For = ");
-var statistics1 = employee.GetStatisticsFor();
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-
-Console.WriteLine("Wynik dla DoWhile = ");
-var statistics2 = employee.GetStatisticsDoWhile();
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-
-Console.WriteLine("Wynik dla While = ");
-var statistics3 = employee.GetStatisticsWhile();
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
